b03d6d86431319785bcfc90f79338c6fff8efe25
prototype1309 <simo.hourri@gmail.com>
2023-06-19 22:59:09 -0400

    console log noteId

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -134,6 +134,8 @@ const handleNoteDelete = (e) => {
 
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  console.log(`Note Deleted! Note ID: ${noteId}`);
+  
 
   if (activeNote.id === noteId) {
     activeNote = {};
