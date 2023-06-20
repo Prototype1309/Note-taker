ac56ec8096bcb63ecf9c2bf5f53c8b9fc27c2bee
prototype1309 <simo.hourri@gmail.com>
2023-06-20 00:12:26 -0400

    deleting the input request function

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -41,3 +41,21 @@ module.exports = (app) => {
       res.send(notesArr);
     });
   });
+      // DELETE REQUEST
+  // Setup the /api/notes/:id delete route
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteId = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      let notesArr = JSON.parse(data);
+      // removes the note with the given id property
+      for (let i = 0; i < notesArr.length; i++) {
+        if (notesArr[i].id === deleteId) {
+          notesArr.splice(i, 1);
+        }
+      }
+      editNote(notesArr);
+      console.log(`Note Deleted! Note ID: ${deleteId}`);
+      res.send(notesArr);
+    });
+  });
