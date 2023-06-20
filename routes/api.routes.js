f32f60884f53f79a026816913cff5fd4e81e7843
prototype1309 <simo.hourri@gmail.com>
2023-06-19 23:55:31 -0400

    set the routing for notes file and db json file

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -5,3 +5,17 @@ const editNote = (updatedNotesArray) => {
     if (err) throw err;
   });
 };
+
+
+// ROUTING
+module.exports = (app) => {
+  // GET REQUEST
+  // Setup the /api/notes GET route
+  app.get("/api/notes", (req, res) => {
+    // Read the db.json file and return all saved notes as JSON.
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      // Parse the JSON string into a JavaScript object
+      res.json(JSON.parse(data));
+    });
+  });1
